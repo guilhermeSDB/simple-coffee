4954fe0cd47f491b6a36fa7e7ffd1c89f953f447
Guilherme S. <guilherme_pikapal@hotmail.com>
2023-11-11 17:32:28 -0300

    fix: netlify bug deploy correction

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -19,4 +19,14 @@ export default <Partial<Config>>{
       },
     },
   },
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+      cssnano:
+        process.env.NODE_ENV === "production"
+          ? { preset: ["default", { discardComments: { removeAll: true } }] }
+          : false, // disable cssnano when not in production
+    },
+  },
 };
